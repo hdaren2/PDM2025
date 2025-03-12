2faf91aa80b7725be56b916aa059100271c38ae1
hdaren2 <163777281+hdaren2@users.noreply.github.com>
2025-03-11 21:01:41 -0500

    Update sketch.js

--- Tone_2.3/sketch.js
+++ Tone_2.3/sketch.js
@@ -21,14 +21,14 @@ function setup() {
     depth: 0.2,
     baseFrequency: 300,
     octaves: 3
-  }).toDestination();
+  }).toDestination().start();;
   noiseEnv = new Tone.AmplitudeEnvelope({
     attack: 1,
     decay: 0.1,
     sustain: 1,
     release: 1
   }).connect(filt);
-  noise = new Tone.Noise("white").connect(noiseEnv);
+  noise = new Tone.Noise("white").connect(noiseEnv).start();;
   values = new Float32Array([-48, -42, -36, -30, -24, -18, -12, -6, 0, -6, -12, -18, -24, -30, -36, -48, -96]);
 }
 
@@ -39,7 +39,6 @@ function draw() {
 }
 
 function mouseClicked() {
-  Tone.start();
   noiseEnv.triggerAttackRelease(30);
   noise.volume.setValueCurveAtTime(values, Tone.now(), 30);
 }
